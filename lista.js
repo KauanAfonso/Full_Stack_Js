f50a78f1489af8912ce0d267a23932600d66ce7c
Kauan <144381805+KauanAfonso@users.noreply.github.com>
2023-12-03 10:05:17 -0300

    Add files via upload

---
+++ lista.js
@@ -0,0 +1,35 @@
+const nome = 'Kauan afonso da silva'
+let letras = "abcdefghijklmnopqrstuvwxyz"
+let espacos = "";
+
+totalLetras = 0
+totalDePalvras = 0
+
+
+for (let l = 0; l < letras.length; l++) {
+    for (let i = 0; i < nome.length; i++) {
+
+        if (nome.toLowerCase()[i] == letras.toLowerCase()[l]) {
+            totalLetras++
+        }
+
+    }
+}
+
+console.log(`Esse nome tem: ${totalLetras} letras`)
+
+
+
+for (let i = 0; i < nome.length; i++) {
+    if (nome[i] == " ") {
+        totalDePalvras += 1
+    }
+}
+
+if (totalDePalvras==0) {
+    totalDePalvras = 1;
+}else {
+    totalDePalvras += 1
+}
+
+console.log(`Essa nome tem ${totalDePalvras}  palavras`)
