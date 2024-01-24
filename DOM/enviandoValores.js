b9d156e391430add76955300674a7da5018c1a7b
Kauan <144381805+KauanAfonso@users.noreply.github.com>
2024-01-24 19:29:24 -0300

    Add files via upload

---
+++ DOM/enviandoValores.js
@@ -0,0 +1,16 @@
+function register(element){
+    const username = element.children.username.value
+    const password = element.children.password.value
+    const passwordConfirmation = element.children.passwordConfirmation.value
+
+    if(password === passwordConfirmation){
+        alert('Usuário' + username + ' registrado !');
+
+    }else{
+        alert('senha incorreta')
+        const paragrafo = document.createElement('p')
+        paragrafo.innerText = 'Senha incorreta'
+        element.appendChild(paragrafo)
+
+    }
+}
