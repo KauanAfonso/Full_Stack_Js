262b727311b3c81d427aea3836ef2a97e897761f
kauanAfonso <kauanafon3@gmail.com>
2024-12-12 08:04:39 -0300

    resolvendo conflitos

--- DOM/correcaoExer1.js
+++ DOM/correcaoExer1.js
@@ -1,37 +1,4 @@
-<<<<<<< HEAD
-function addPlayer() {
-    const posicao = document.getElementById('position').value
-    const name = document.getElementById('name').value
-    const number = document.getElementById('number').value
 
-    const confirmacao = confirm("Escalar " + name + " como " + posicao + "?")
-
-    if(confirmacao){
-        const teamList = document.getElementById('timeList')
-        const playerItem = document.createElement('li')
-        playerItem.id = 'player-' + number
-        playerItem.innerText = posicao + " : " + name + "(" + number + ")"
-        teamList.appendChild(playerItem)
-
-        //Depois de enviar o formulario , deixar ele em branco
-        document.getElementById('postion').value = ''
-        document.getElementById('name').value = ''
-        document.getElementById('number').value = ''
-    }
-}
-
-
-function removePlayer(){
-    const number = document.getElementById('numberRemove').value
-    const playerToRemove = document.getElementById('player-' + number)
-
-    const confirmacao = confirm('Remover o jogador ' + playerToRemove.innerText + "?")
-
-    if(confirmacao){
-       playerToRemove.remove()
-        document.getElementById('numbeRemove').value = ''
-    }
-=======
 function addPlayer() {
     const posicao = document.getElementById('position').value
     const name = document.getElementById('name').value
--- DOM/enviandoValores.js
+++ DOM/enviandoValores.js
@@ -1,20 +1,4 @@
-<<<<<<< HEAD
-function register(element){
-    const username = element.children.username.value
-    const password = element.children.password.value
-    const passwordConfirmation = element.children.passwordConfirmation.value
 
-    if(password === passwordConfirmation){
-        alert('Usuário' + username + ' registrado !');
-
-    }else{
-        alert('senha incorreta')
-        const paragrafo = document.createElement('p')
-        paragrafo.innerText = 'Senha incorreta'
-        element.appendChild(paragrafo)
-
-    }
-=======
 function register(element){
     const username = element.children.username.value
     const password = element.children.password.value
