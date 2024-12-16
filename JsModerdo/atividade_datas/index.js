f18b680044a565688a45aa79457a76f9251ff195
kauanAfonso <kauanafon3@gmail.com>
2024-12-16 09:15:19 -0300

    Formatando data

--- JsModerdo/atividade_datas/index.js
+++ JsModerdo/atividade_datas/index.js
@@ -1,1 +1,28 @@
-const daysjs = require('dayjs')
+let dayjs = require('dayjs')
+
+
+data = dayjs("12-25-2001").format("DD-MM-YYYY");
+
+console.log(data);
+
+
+
+// dia_de_hoje = dayjs().date() 
+// console.log(dia_de_hoje)
+
+
+// dayjs().format()
+// console.log(dayjs().year())
+
+
+// const quantos_dias_aniversario = (data) =>{
+//     const data_de_aniversario = dayjs(data).locale('pt-br').format();
+//     console.log(data_de_aniversario)
+
+//     ano_atual = dayjs().year()
+//     mes_atual = dayjs().month()
+//     dia_atual = dayjs().date()
+    
+// }
+
+// quantos_dias_aniversario('28-09-2006')
