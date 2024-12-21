d6b56272bdf7456c485734f96d25f37f6a835563
KauanAfonso <kauanafon3@gmail.com>
2024-12-21 08:52:33 -0300

    aula de loaders

--- JsModerdo/webpack/src/webpack.config.js
+++ JsModerdo/webpack/src/webpack.config.js
@@ -2,12 +2,27 @@ const path = require('path'); // Módulo para trabalhar com caminhos
 
 module.exports = {
     entry: {
-        main: './index.js', // Definindo o ponto de entrada
-        hello: './hello.js'
+        main: './index.js'// Definindo o ponto de entrada
     },
-    mode: 'development', // Configurando para o modo de desenvolvimento
-    output: {
-        path: path.resolve(__dirname, 'public'), // Caminho completo para a saída
-        filename: '[name].bundle.min.js' // Nome correto do arquivo de saída, o [name] vai diferenciar quando tem mais de uma entrada para processar a saida
+    mode:'development',
+    module:{
+        rules:[{
+            test: /\.css$/,
+            use: ['style-loader' , 'css-loader']
+        }]
     }
 };
+
+
+// module.exports = {
+//     entry: {
+//         main: './index.js', // Definindo o ponto de entrada
+//         hello: './hello.js'
+//     },
+//     mode: 'development', // Configurando para o modo de desenvolvimento
+//     output: {
+//         path: path.resolve(__dirname, 'public'), // Caminho completo para a saída
+//         filename: '[name].bundle.min.js' // Nome correto do arquivo de saída, o [name] vai diferenciar quando tem mais de uma entrada para processar a saida
+//     }
+// };
+
