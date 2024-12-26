be10cf8d6999959abae5f384f293a04034afe4ef
KauanAfonso <kauanafon3@gmail.com>
2024-12-26 11:06:00 -0300

    enviando aula de classes e objetos

---
+++ POO/classe_e_objetos/Book.js
@@ -0,0 +1,18 @@
+class Book{
+    constructor(title){
+        this.title = title
+        this.published = false
+    }
+
+    publish(){
+        this.published = true
+    }
+
+
+}
+
+const eragon = new Book("Eragon")
+eragon.published()
+
+console.log(eragon)
+console.log(eragon instanceof)
