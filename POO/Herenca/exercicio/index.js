366789ff8b4c8892e29866be4d25222fa31db5d8
KauanAfonso <kauanafon3@gmail.com>
2024-12-30 20:01:18 -0300

    exercicio de herença

--- POO/Herenca/exercicio/index.js
+++ POO/Herenca/exercicio/index.js
@@ -1,41 +1,77 @@
 class Component{
-    #id 
-    constructor(component, area){
-        this.#id = id
-        this.component = component
-        this.build()
-        this.area = area
+     #component
+    constructor(component, area, id) {
+        this.id = id;
+        this.#component = component;
+        this.area = document.getElementById(area);
+        this.variavel = this.build();  // Define variavel imediatamente
     }
 
-    build = ()=>{
-        component.id = this.#id
-        return  document.createElement(component)
+    build() {
+        const el = document.createElement(this.#component);
+        el.id = this.id;  // Define o id diretamente
+        return el;
     }
 
     get_value_attribute(){
-        return this.component.value
+        return this.#component.value
     }
 
     get_id(){
-        return this.#id
+        return this.id
     }
 
     render(){
-        this.area.appendChild(this.component)
+        this.area.appendChild(this.variavel);
     }
 }
 
-class input extends Component{
-    constructor(type,placeholder, component){
-        super(component)
-        this.type
-        this.placeholder
+class Input extends Component{
+    constructor(type, placeholder, id, area){
+        super("input",area, id)
+        this.type = type
+        this.placeholder = placeholder
+        this.set_atibutte()
     }
 
-    build(){
-        const input = document.createElement('input')
-        input.type = this.type
-        input.setAttribute('placeholder' , this.placeholder)
+    set_atibutte(){
+        this.variavel.type = this.type;
+        this.variavel.setAttribute('placeholder', this.placeholder);
     }
 
 }
+
+
+class Label extends Component{
+    constructor(label_for, content, id, area){
+        super("Label", area,id)
+        this.label_for = label_for
+        this.content = content
+        this.set_atibutte_label()
+        this.set_content()
+    }
+
+    set_atibutte_label(){
+        this.variavel.setAttribute('for', this.label_for)
+      
+    }
+
+    set_content(){
+        this.variavel.textContent = this.content
+      
+    }
+
+    
+}
+
+// class form extends Component{
+//     constructor()
+// }
+
+
+let new_label = new Label("input-01", "Isso é um teste: ", "label01", 'area')
+new_label.render()
+
+let input_teste = new Input("text", "Digite algo", "input-01", 'area')
+input_teste.render()
+
