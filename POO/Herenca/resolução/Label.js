e9f9edf81a9eed96ad718b08184b81a0ee432a53
KauanAfonso <kauanafon3@gmail.com>
2025-01-01 11:46:27 -0300

    resolução de exercicio

--- POO/Herenca/resolução/Label.js
+++
@@ -1,7 +0,0 @@
-import { Component } from "./Component";
-
-export class Label extends Component{
-    constructor(text, parent, options){
-        super('label', parent, Object.assign({},options, {textContent: text}))
-    }
-}
