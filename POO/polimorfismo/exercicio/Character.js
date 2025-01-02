1d496d10de0482bde7693af2c49a9582082529d5
KauanAfonso <kauanafon3@gmail.com>
2025-01-02 18:37:42 -0300

    Exercicio de poliformismo

--- POO/polimorfismo/exercicio/Character.js
+++ POO/polimorfismo/exercicio/Character.js
@@ -1,4 +1,4 @@
-class Character{
+export class Character{
     constructor(nome, vida, ataque, defesa){
         this.nome = nome 
         this.vida = vida 
---
+++ POO/polimorfismo/exercicio/Thief.js
@@ -0,0 +1,15 @@
+import { Character } from "./Character";
+
+class Thief extends Character{
+    constructor(nome, vida, ataque, defesa){
+        super(nome, vida, ataque, defesa)
+    }
+
+    attack(other_character){
+        if(other_character instanceof Character){
+            other_character.vida -= (2 * this.ataque) - other_character.defesa
+        }else{
+            return "isn't instance of class"
+        }
+    }
+}
