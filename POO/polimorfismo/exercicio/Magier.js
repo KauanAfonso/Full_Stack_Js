46a90c399d62de68079bfe4aae28c576f286e104
KauanAfonso <kauanafon3@gmail.com>
2025-01-02 18:44:02 -0300

    aula de polimorfismo

---
+++ POO/polimorfismo/exercicio/Magier.js
@@ -0,0 +1,24 @@
+import { Character } from "./Character";
+
+class Magier extends Character{
+    constructor(nome, vida, ataque, defesa, magia){
+        super(nome,vida,ataque,defesa)
+        this.magia = magia
+    }
+
+    attack(other_character){
+        if(other_character instanceof Character){
+            other_character.vida -= (this.ataque + this.magia) - other_character.defesa
+        }else{
+            return "isn't instance of class"
+        }
+    }
+
+    heal_characther(other_character){
+        if(other_character instanceof Character){
+            other_character.vida = other_character.vida + ( 2 * this.magia)
+        }else{
+             return "isn't instance of class"
+        }
+    }
+}
