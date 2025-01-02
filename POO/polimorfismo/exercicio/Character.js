b9f597ddcc58023e1fc2f3f3de0f76790b6fbb84
KauanAfonso <kauanafon3@gmail.com>
2025-01-02 17:55:05 -0300

    exercicio poliformismo

---
+++ POO/polimorfismo/exercicio/Character.js
@@ -0,0 +1,27 @@
+class Character{
+    constructor(nome, vida, ataque, defesa){
+        this.nome = nome 
+        this.vida = vida 
+        this.ataque = ataque
+        this.defesa = defesa
+    }
+
+    attack(other_character){
+        if(other_character instanceof Character){
+            other_character.vida -= this.ataque - other_character.defesa
+        }else{
+            return "isn't instance of class"
+        }
+    }
+
+    return_life(){
+        return this.vida
+    }
+}
+
+
+let kauan = new Character("Kauan", 200, 100, 100)
+let cris = new Character('Cris', 400 , 100, 20)
+
+kauan.attack(cris)
+console.log(cris.return_life())
