ac8e2359a278dfad79d54e24ff9253f7c001cc82
KauanAfonso <kauanafon3@gmail.com>
2025-01-02 21:45:04 -0300

    exercicio de polimorfismo

--- POO/polimorfismo/exercicio/Warrior.js
+++ POO/polimorfismo/exercicio/Warrior.js
@@ -1,5 +1,30 @@
 import { Character } from "./Character";
 
 class Warrior extends Character{
-    
+    constructor(nome, vida, ataque, defesa, escudo, posicao){
+        super(nome, vida, ataque, defesa)
+
+        this.escudo = escudo
+        this.posicao = "Defense"
+    }
+
+    change_position(){
+        if(this.posicao == "Attack"){
+            this.defesa += this.escudo
+            return this.posicao = "Defense"
+
+        }else{
+            this.defesa -= this.escudo
+             return this.posicao = "Attack"
+        }
+    }
+
+    attack(other_character){
+        if(other_character instanceof Character && this.posicao == "Attack"){
+            other_character.vida -= this.ataque - other_character.defesa
+        }else{
+            return "isn't instance of class or position incorrect"
+        }
+    }
+
 }
