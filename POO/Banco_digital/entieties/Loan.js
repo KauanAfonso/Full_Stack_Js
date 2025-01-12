d21517b1ece050c7fb3138d4e47c4892214b55df
KauanAfonso <kauanafon3@gmail.com>
2025-01-12 11:03:04 -0300

    Atulizando as classes

--- POO/Banco_digital/entieties/Loan.js
+++ POO/Banco_digital/entieties/Loan.js
@@ -1,3 +1,22 @@
-class Loan{
+module.exports = class Loan{
+  
+    static #juros = 10
+
+    constructor(valor, data, quantidade_parcelas){
+        this.valor = valor
+        this.data = data
+        this.quantidade_parcelas = quantidade_parcelas
+
+    }
+
+    static get verificar_juros(){
+        return Loan.#juros
+    }
+
+    static set definir_juros(porcentagem){
+
+        aumento = (Loan.#juros * porcentagem) / 100
+        return Loan.#juros += aumento
+    }
 
 }
