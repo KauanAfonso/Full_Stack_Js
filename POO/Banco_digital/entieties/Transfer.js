a104054ff30663c1eb0e96487a1990f01fc86bb3
KauanAfonso <kauanafon3@gmail.com>
2025-01-12 10:42:59 -0300

    Atulizando as classes

--- POO/Banco_digital/entieties/Transfer.js
+++ POO/Banco_digital/entieties/Transfer.js
@@ -1,3 +1,10 @@
-class Transfer{
-    constructor()
+import { User } from "./User";
+
+module.exports = class Transfer{
+    constructor(usuário_que_enviou_transferencia, usuário_que_recebeu_transferencia,valor,data){
+        this.usuário_que_enviou_transferencia = usuário_que_enviou_transferencia
+        this.usuário_que_recebeu_transferencia = usuário_que_recebeu_transferencia
+        this.valor = valor
+        this.data = data
+    }
 }
---
+++ POO/Banco_digital/entieties/User.js
@@ -0,0 +1,7 @@
+module.exports = class User{
+    constructor(nome_completo, email, conta){
+        this.nome_completo = nome_completo
+        this.email = email
+        this.conta = conta
+    }
+}
