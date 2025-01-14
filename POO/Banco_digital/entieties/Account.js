ccfae9b258289b399f11cf9c175ca9d351c7110d
KauanAfonso <kauanafon3@gmail.com>
2025-01-14 09:24:28 -0300

    Criando classe Account

--- POO/Banco_digital/entieties/Account.js
+++ POO/Banco_digital/entieties/Account.js
@@ -1,3 +1,26 @@
 class Account{
-    
+    #saldo
+    constructor(user){
+        this.user = user
+        this.#saldo = 0
+        this.depositos_totais = []
+        this.empréstimos_totais = []
+        this.transferencias_totais = []
+    }
+
+    get obter_saldo(){
+        return this.#saldo
+    }
+
+    depositar_conta(deposito){
+            this.#saldo += deposito
+            this.depositos_totais.push(deposito)
+    }
+
+    emprestimo_conta(emprestimo){
+        this.#saldo += emprestimo
+        this.empréstimos_totais.push(emprestimo)
+    }
+
+    trasnferencias_conta(trasnferencias)
 }
