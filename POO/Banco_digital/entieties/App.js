c47bf57006f9f924484d9a56bd130ca1f339c8f1
KauanAfonso <kauanafon3@gmail.com>
2025-01-15 22:38:41 -0300

    classe app

--- POO/Banco_digital/entieties/App.js
+++ POO/Banco_digital/entieties/App.js
@@ -4,9 +4,12 @@ class App{
  
     static #usuarios = []
 
+    static verificar_user_existente(email){
+        return App.#usuarios.filter((user) => user.email === email )
+    }
 
     static criar_usuario(nome_completo, email, conta){
-        const usuário_existe = App.#usuarios.filter((user) => user.email === email )
+        const usuário_existe = this.verificar_user_existente(email)
         if(usuário_existe){
             return `Usuário já existente`
         }else{
