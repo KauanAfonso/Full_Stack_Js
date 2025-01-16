b0fbf4e39589d8346b65a984f3434b3152c9cf37
KauanAfonso <kauanafon3@gmail.com>
2025-01-15 22:37:14 -0300

    classe app

--- POO/Banco_digital/entieties/App.js
+++ POO/Banco_digital/entieties/App.js
@@ -1,3 +1,20 @@
+const User = require("./User")
+
 class App{
+ 
+    static #usuarios = []
+
+
+    static criar_usuario(nome_completo, email, conta){
+        const usuário_existe = App.#usuarios.filter((user) => user.email === email )
+        if(usuário_existe){
+            return `Usuário já existente`
+        }else{
+            let usuario = new User(nome_completo, email,conta)
+            App.#usuarios.push(usuario)
+        }
+
+
+    }
     
 }
