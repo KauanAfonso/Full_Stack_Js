9f50ad415a8c61b3eee1a39f0f98a382213ba260
kauan <kauan>
2025-02-01 10:42:50 -0300

    aaa

--- API/tratamento_de_erros/index.js
+++ API/tratamento_de_erros/index.js
@@ -1,3 +1,4 @@
+
 function sum(a, b) {
     const firstNumber = Number(a)
     const secondNumber = Number(b)
@@ -9,8 +10,14 @@ function sum(a, b) {
     return firstNumber + secondNumber
 }
 
-console.log(sum(2, 9))
-console.log(sum(true, 14))
-console.log(sum(undefined, 22))
-console.log(sum(18, "0"))
-console.log(sum(39, null))
+try{
+    console.log(sum(2, 9))
+    console.log(sum(true, 14))
+    console.log(sum(undefined, 22))
+    console.log(sum(18, "0"))
+}catch(erro){
+    console.log("Erro: ", erro)
+}finally{
+    console.log("Finalizado")
+}
+
