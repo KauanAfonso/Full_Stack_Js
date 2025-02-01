68b6c30e22e47d548fe64d0b908f92d4e7ece38e
kauan <kauan>
2025-02-01 10:34:02 -0300

    Teste

--- API/tratamento_de_erros/index.js
+++ API/tratamento_de_erros/index.js
@@ -14,7 +14,7 @@ try {
     console.log(sum(2, 9))
     console.log(sum(true, 14))
     // console.log(sum(undefined, 22))
-    //testeeeeeeeeeeeeeeeeee
+    
     console.log(sum(18, "0"))
 
 }catch(erro){
