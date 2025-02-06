7cf049c9dd9f2ff457f05c2cd0e3502bd709d41d
kauan <kauanafon3@gmail.com>
2025-02-05 22:33:15 -0300

    Testando

--- POO/exercicio/index.js
+++ POO/exercicio/index.js
@@ -1,7 +1,12 @@
-class hotel{
+class Hotel{
     constructor(this, nome ,enderco, total_quartos){
         this.nome = nome
         this.enderco = enderco
         this.total_quartos = total_quartos
     }
+
+
 }
+
+
+let hotel = new Hotel
