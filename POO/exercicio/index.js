c54bb1d891382cad49a5404a8257bd92bfc515bc
KauanAfonso <KauanAfonso>
2025-02-05 22:42:22 -0300

    exercicio simples

--- POO/exercicio/index.js
+++ POO/exercicio/index.js
@@ -1,12 +1,19 @@
 class Hotel{
-    constructor(this, nome ,enderco, total_quartos){
+    constructor( nome ,enderco, total_quartos, taxa){
         this.nome = nome
         this.enderco = enderco
         this.total_quartos = total_quartos
+        this.taxa = taxa
+    }
+
+
+    agendar_diaria(dias){
+        return dias * this.taxa
     }
 
 
 }
 
 
-let hotel = new Hotel()
+let hotel = new Hotel("hotel", "Rua 2", 55, 5.55)
+console.log(hotel.agendar_diaria(7))
