651927a918d152e320ed8d0b2f6d72fd8baa4ca5
KauanAfonso <kauanafon3@gmail.com>
2025-03-02 00:05:23 -0300

    pegando a localidade do celular

--- API/expressoes_regulares/index.js
+++ API/expressoes_regulares/index.js
@@ -1,6 +1,8 @@
 function phoneNumber(phoneNumberSring){
     const fixedString = phoneNumberSring.replace(/[\sa-zA-Z]/g, "")
-    this.contryCode = fixedString.match(/(?<=\+)\d{1,3}/)[0]
+    this.countryCode = fixedString.match(/(?<=\+)\d{1,3}/)[0]
+    this.ddd = fixedString.match(/(?<=\()\d+(?=\))/)[0]
+    this.number = fixedString.match(/(?<=\)).+/)[0].replace(/-/g, "")
 }
 
 console.log(new phoneNumber('+55 (22) 9 9876-2514'))
