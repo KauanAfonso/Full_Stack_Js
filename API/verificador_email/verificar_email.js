20959b4b3da47e986c73cacde4e79f11c9c7dbc3
KauanAfonso <kauanafon3@gmail.com>
2025-03-03 16:25:15 -0300

    Preparando exercicio

--- API/verificador_email/verificar_email.js
+++ API/verificador_email/verificar_email.js
@@ -1,3 +1,18 @@
-const verificar_email = () =>{
-    
+/*
+
+- Um caractere @
+- Texto antes do @ com pelo menos 2 caracteres, números ou _ (underscore).
+- Texto após o @ com pelo menos 2 caracteres
+- Um . seguido de mais texto com pelo menos 2 caracteres após o texto após o @
+- Estrutura de um email válido: **xx@xx.xx**
+
+*/
+
+const verificar_email = (email)=>{
+    const emailfixado = email.replace()
+    console.log(emailfixado)
+}
+
+const verificador_senha = () =>{
+
 }
