f308c55acc8563de5c564bf7788fbac022be1cfb
KauanAfonso <kauanafon3@gmail.com>
2025-03-15 23:15:20 -0300

    exercicio de expressao regular feito com o instutor

--- API/verificador_email/verificar_email.js
+++ API/verificador_email/verificar_email.js
@@ -1,22 +1,51 @@
-/*
-
-- Um caractere @
-- Texto antes do @ com pelo menos 2 caracteres, números ou _ (underscore).
-- Texto após o @ com pelo menos 2 caracteres
-- Um . seguido de mais texto com pelo menos 2 caracteres após o texto após o @
-- Estrutura de um email válido: **xx@xx.xx**
-
-*/
-
-const verificar_email = (email)=>{
-    const emailfixado = email.replace(/\s/g, '');
-    const email_arroba = emailfixado.match(/[/@]/)
-    console.log(email_arroba)
-    return emailfixado
+function validateEmail(email) {
+    if (!email.match(/\w{2,}@[a-zA-Z]{2,}\.[a-zA-Z]{2,}/)) {
+        const err = new Error('Email inválido.')
+        err.input = 'email'
+        throw err
+    }
 }
 
-const verificador_senha = (senha) =>{
-    return senha
+function validatePassword(password) {
+    if (
+        password.length < 8 || 
+        !password.match(/[a-z]/) || 
+        !password.match(/[A-Z]/) || 
+        !password.match(/[0-9]/) ||
+        !password.match(/[^a-zA-Z0-9\s]/)
+    ) {
+        const err = new Error('Senha inválida.')
+        err.input = 'password'
+        throw err
+    }
 }
 
-console.log(verificar_email('kauana afonso da silva'))
+function resetFormStyles() {
+    Object.entries(userInputs).forEach(([key, value]) => {
+        value.classList.remove('success', 'error')
+        document.querySelector(`#${key}-error`).textContent = ''
+    })
+}
+
+const userInputs = {}
+
+userInputs.name = document.querySelector('#name')
+userInputs.email = document.querySelector('#email')
+userInputs.password = document.querySelector('#password')
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (ev) => {
+    ev.preventDefault()
+    resetFormStyles()
+    try {
+        userInputs.name.classList.add('success')
+        validateEmail(userInputs.email.value)
+        userInputs.email.classList.add('success')
+        validatePassword(userInputs.password.value)
+        userInputs.password.classList.add('success')
+    } catch (err) {
+        userInputs[err.input].classList.add('error')
+        document.querySelector(`#${err.input}-error`).textContent = err.message
+    }
+})
