bfe929bd5d7fe34e2d3725fda40ff3a35fa5d5e8
KauanAfonso <kauanafon3@gmail.com>
2025-04-06 00:32:59 -0300

    Rejeitando promisse de async

--- API/ASYNC/index.js
+++ API/ASYNC/index.js
@@ -1,9 +1,15 @@
 //O async retorna uma promessa automaticamente
 async function asyncSum(a, b) {
+    if(typeof a !== 'number' || typeof b !== 'number'){
+        return Promise.reject("Arguments must be of type number")
+    }
     return a + b
 }
 
 async function asyncSubtract(a, b) {
+    if(typeof a !== 'number' || typeof b !== 'number'){
+        return Promise.reject("Arguments must be of type numberr")
+    }
     return a - b
 }
 
