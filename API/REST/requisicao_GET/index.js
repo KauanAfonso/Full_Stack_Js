0a37667128cc89b7774e77f6f656449974720d0d
KauanAfonso <kauanafon3@gmail.com>
2025-04-09 21:04:04 -0300

    Comentando

--- API/REST/requisicao_GET/index.js
+++ API/REST/requisicao_GET/index.js
@@ -1,7 +1,11 @@
-function createContryCard(country){
-    const card = document.createElement('div')
-    card.classList.add('country')
 
+//Fução para criar um card de pais
+function createContryCard(country){
+    const card = document.createElement('div') //div mae
+    card.classList.add('country') //add a classe contry
+
+    //Para cada elemento visivel pegar ele na api, criar 
+    //elmento html 
     const countryName = country.name.common
     const name = document.createElement('h2')
     name.textContent = countryName
@@ -14,12 +18,14 @@ function createContryCard(country){
     const continent = document.createElement('p')
     continent.textContent = "Continent: " + ContryContinent
 
+    //renderizando
     card.append(name, flag, continent)
     document.querySelector('#countries').append(card)
 }
 
 
-
+//Essa função  async está pegando os dados da api
+//retornando em json e por cada um dos elements chamando o createContryCard
 async function getCountries() {
     const response = await fetch('https://restcountries.com/v3.1/all') //o fetch é usado para requisição 
     const countries = await response.json()
