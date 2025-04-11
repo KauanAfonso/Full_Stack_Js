5d90d8bef8c07b965691b4b6754ac5e51a60502b
KauanAfonso <kauanafon3@gmail.com>
2025-04-10 22:46:15 -0300

    Aula de requisição post parte 2

--- API/REST/requisicao_POST/index.js
+++ API/REST/requisicao_POST/index.js
@@ -1,3 +1,6 @@
+
+
+//Função que cria um artigo
 function article(articleData){
     const article = document.createElement('article')
     article.classList.add('article')
@@ -17,7 +20,7 @@ function article(articleData){
 
 
 
-
+//Função que renderiza o blog
 async function toRenderArticle(url) {
     const response = await fetch(url)
     data = await response.json()
@@ -27,3 +30,28 @@ async function toRenderArticle(url) {
 }
 
 toRenderArticle('http://localhost:3000/articles')
+
+//FAZENDO REQUISIÇÂO POST
+
+const form = document.querySelector('form')
+form.addEventListener('submit', async (ev) =>{
+    ev.preventDefault()
+
+    const articleData = {
+        title:document.querySelector('#title').value,
+        author: document.querySelector("#author").value,
+        content:document.querySelector("#content").value
+    }
+
+    const response = await fetch('http://localhost:3000/articles', {
+        method:"POST", //definindo que sera post
+        headers:{
+            'Content-type': 'application/json' //definindo o formato de comunicação
+        },
+        body: JSON.stringify(articleData) //representa oq vamos mandar 
+    }) 
+
+    const savedArticle = await response.json()
+    form.reset() //limpar form
+    toRenderArticle('http://localhost:3000/articles')
+})
