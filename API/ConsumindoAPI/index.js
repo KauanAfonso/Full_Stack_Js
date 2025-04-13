eca63269fb6bcf9cceeed0fa6e067268e1d48fdd
KauanAfonso <kauanafon3@gmail.com>
2025-04-13 11:44:35 -0300

    CRUD realizado

--- API/ConsumindoAPI/index.js
+++ API/ConsumindoAPI/index.js
@@ -11,10 +11,9 @@ ideia -> crie um ink na renderização para air para o endpoint diretor atraves 
 function renderTransactions(data){
     const div_main = document.createElement('div')
 
-    const label = document.createElement('label')
-    label.textContent = "Name: "
+
     const name_transaction = document.createElement('h4');
-    name_transaction.textContent = data.name;
+    name_transaction.textContent = `Nome da transação: ${data.name}`;
 
     const input_id = document.createElement('input');
     input_id.setAttribute('type','hidden');
@@ -22,19 +21,31 @@ function renderTransactions(data){
     input_id.id = `transaction ${data.id}`
 
 
-    const label2 = document.createElement('label')
-    label2.textContent = "value: "
+
     const value_transaction = document.createElement('p');
-    value_transaction.textContent = data.value;
+    value_transaction.textContent = `Value: ${data.value}`;
 
     const btn_delete = document.createElement('button')
     btn_delete.textContent = "Excluir";
+    btn_delete.classList ="btn btn-danger";
     btn_delete.id = `Excluir ${data.id}`
 
-    div_main.append(label, input_id, name_transaction,label2, value_transaction,btn_delete)
-    document.getElementById('transactions').append(div_main)
 
-    btn_delete.addEventListener('click',()=>delete_transaction(`http://localhost:3000/transaction/${data.id}`))
+    const btn_update = document.createElement('button')
+    btn_update.setAttribute('data-bs-toggle', 'modal')
+    btn_update.setAttribute
+    ('data-bs-target', '#staticBackdrop')
+    btn_update.classList = "btn btn-primary"
+    btn_update.textContent = "Update"
+
+    div_main.append(input_id, name_transaction, value_transaction,btn_update ,btn_delete)
+    document.getElementById('transactions').append(div_main)
+    
+    const url = `http://localhost:3000/transaction/${data.id}`
+
+    btn_delete.addEventListener('click',()=> delete_transaction(url) )
+    btn_update.addEventListener('click',()=> form_uptade(url,data) )  
+
 }
 
 
@@ -68,19 +79,57 @@ const form = document.getElementById('form')
 
         const responseData = await response.json()
         form.reset()
+        location.reload() 
         getTransactions('http://localhost:3000/transaction')
     
     })
 
 
+async function update_transaction(url) {
+
+    const info = {
+        name:document.getElementById('name_update').value,
+        value:document.getElementById('value_update').value,
+    }
+
+    const response = await fetch(url,{
+        method:'PUT',
+        headers:{
+            "Content-type":'Application/json'
+        },
+        body:JSON.stringify(info)
+    })
+
+
+
+    const response_data = await response.json()
+    console.log(response_data)
+}
+
+const form_uptade = (url, info) =>{
+    let form = document.getElementById('form_update')
+    form.action = url
+   
+    document.getElementById('name_update').value = info.name,
+    document.getElementById('value_update').value = info.value,
+    
+    
+    form.addEventListener('submit', (ev)=>{
+        ev.preventDefault()
+        update_transaction(url)
+        location.reload()
+    })
+    
+}
+
 async function delete_transaction(url){
         const response = await fetch(url,{
             method:'DELETE',
             headers:{
-                "Content-type": "application/json"
+                "Content-type": "Application/json"
             },
         });
 
         alert('DELETADO COM SUCESSO')
-        getTransactions('http://localhost:3000/transaction')
+        location.reload()       
 }
