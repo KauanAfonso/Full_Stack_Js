8fb0be6e58abe8ef6f617c62c437f68ebf8dc039
KauanAfonso <kauanafon3@gmail.com>
2025-04-27 01:52:17 -0300

    Tuplas

---
+++ Typescript/tuplas/tuplas.ts
@@ -0,0 +1,14 @@
+
+
+let crew: [string, string, string] //tupla -> limitou o array e tipou
+
+crew[0] = "Kauan"
+crew[1] = "Kauan"
+crew[2] = "Kauan"
+
+
+//Exemplos com desestruturação
+let point: [number, number]
+point = [2, 10]
+
+let [x, y] = point
